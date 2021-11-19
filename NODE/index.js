47d3a60bcab6166f48a7e44db3cf6c1eb3292293
anhnn02 <anhnn02.it@gmail.com>
2021-11-19 18:30:41 +0700

    update

--- NODE/index.js
+++ NODE/index.js
@@ -1,8 +1,58 @@
 const express = require('express')
 const app = express()
- 
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./passport-setup');
+
+app.use(cors());
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cookieSession({
+  name: 'tuto-session',
+  keys: ['key1', 'key2']
+}))
+
+const isLoggedIn = (req, res, next) =>{
+  if(req.user){
+    next();
+  } else{
+    res.sendStatus('401');
+  }
+}
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('You logout!')
+})
+app.get('/failed', function (req, res) {
+  res.send('You failed')
+})
+app.get('/good', isLoggedIn, function (req, res) {
+  res.send('Welcome hello ${red.user.email}')
 })
  
+app.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/good');
+  });
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+})
+
 app.listen(3000)
---
+++ NODE/passport.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(user, done) {
+    done(err, user);
+});
+
+  
+passport.use(new GoogleStrategy({
+    clientID: "831901708936-kplk3q8n4rffk2cmjpojqoerjvuetsj1.apps.googleusercontent.com",
+    clientSecret: "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",
+    callbackURL: "http://localhost:3000/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+  }
+));
